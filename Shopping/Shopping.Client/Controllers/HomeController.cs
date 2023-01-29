98b03177e10ffa247fc5136cd0f0fa3df065840e
Mykyta <dunik22222@gmail.com>
2023-01-29 04:50:44 +0200

    init

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Models;
+using System.Text.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using Shopping.Client.Data;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
